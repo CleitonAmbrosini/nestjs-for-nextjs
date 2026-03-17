8aa5379aa375dcccccb7d23ac8a0a978f5a919a5
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2026-03-16 23:18:52 -0300

    feat: create auth dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import type { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  login() {
-    return 'Hello from login';
+  login(loginDto: LoginDto) {
+    console.log(loginDto);
+    return loginDto;
   }
 }
