0b9893c9802a54c4ec228941b0bee761332fd800
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2026-03-19 23:47:14 -0300

    feat: add decorators on DTO data

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
+import {IsEmail, IsNotEmpty, IsString } from "class-validator";
+
 export class LoginDto {
+  @IsEmail({}, { message: 'Invalid email' })
   email: string;
+
+  @IsString({ message: 'Password must be a string' })
+  @IsNotEmpty({ message: 'Password is required' })
   password: string;
 }
