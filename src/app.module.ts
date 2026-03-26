ce612c8e18e0be28a813b3721ce5affd4177e314
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2026-03-25 23:45:44 -0300

    feat: add typeorm and create environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UserModule, PostModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    PostModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: Boolean(process.env.DB_AUTO_LOAD_ENTITIES),
+      synchronize: Boolean(process.env.DB_SYNCHRONIZE),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
   exports: [],
