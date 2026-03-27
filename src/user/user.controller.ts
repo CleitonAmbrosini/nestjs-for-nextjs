928e21636137df5ce4579ab3bbc638b25430584d
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2026-03-26 23:30:33 -0300

    feat: create route to create a user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,19 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import CustomParseIntPipe from 'src/common/pipe/custom-parse-int-pipe.pipe';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Get(':id')
   findOne(@Param('id', CustomParseIntPipe) id: number) {
-    return `User ${id}`
+    return `User ${id}`;
+  }
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
+import { HashingService } from 'src/common/hashing/hashing.service';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly hashingService: HashingService,
   ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const existEmail = await this.userRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+
+    if (existEmail) {
+      throw new ConflictException('Email already exists');
+    }
+
+    const hashedPassword = await this.hashingService.hash(createUserDto.password);
+    const newUser: CreateUserDto = {
+      name: createUserDto.name,
+      email: createUserDto.email,
+      password: hashedPassword,
+    };
+
+    const user = await this.userRepository.save(newUser);
+    return user;
+  }
 }
